24fb0b1ac4e7b27bfa480474cdbb56b4b41d387d
Ayush Gupta <ayushgupta.silversky@gmail.com>
2026-03-25 15:21:58 +0530

    find all User | find a particular User

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -15,4 +15,22 @@ export class UsersService {
 
     return this.repo.save(user);
   }
+
+  async findOne(id: number) {
+    const user = await this.repo.findOneBy({ id });
+    if (!user)
+      throw new NotFoundException(`User with user-id: ${id} not found`);
+    console.log(user);
+    return user;
+  }
+
+  async find() {
+    const user = await this.repo.find();
+    console.log(user);
+    return user;
+  }
+
+  udapte() {}
+
+  remove() {}
 }
