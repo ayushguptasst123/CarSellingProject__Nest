07b87b2618bfae6289a76dfd8fbf96a2f703997a
Ayush Gupta <ayushgupta.silversky@gmail.com>
2026-03-25 13:13:51 +0530

    Adding Lifecycle Hooks Decorator

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,11 @@
-import { Column, Entity, PrimaryGeneratedColumn, Unique } from 'typeorm';
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
@@ -10,4 +17,20 @@ export class User {
 
   @Column()
   password: string;
+
+  // This is hook decorator
+  @AfterInsert()
+  logInsert() {
+    console.log(`Inserted User with id: ${this.id}`);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log(`User with id: ${this.id} is updated`);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log(`User with id: ${this.id} is removed Successfully`);
+  }
 }
