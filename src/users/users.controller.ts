f56e507bf384df70f40df31fc009257242555cc0
Ayush Gupta <ayushgupta.silversky@gmail.com>
2026-03-25 18:37:30 +0530

    Exclude password field from response

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,13 @@
 import {
   Body,
+  ClassSerializerInterceptor,
   Controller,
   Delete,
   Get,
   Param,
   Patch,
   Post,
+  UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user-dto';
 import { UsersService } from './users.service';
@@ -15,6 +17,7 @@ import { UpdateUserDto } from './dtos/update-user-dto';
 export class UsersController {
   constructor(private userService: UsersService) {}
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get('/:id')
   findUser(@Param('id') id: string) {
     return this.userService.findOne(Number(id));
